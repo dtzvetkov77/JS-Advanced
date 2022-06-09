23ff4e2c5416eea5f6fdd08a1d75ea167609bc8a
Daniel Tsvetkov <98685589+dtzvetkov77@users.noreply.github.com>
2022-06-09 17:44:24 +0300

    Delete DOM-Introduction-Exercise directory

--- DOM-Introduction-Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++
@@ -1,9 +0,0 @@
-function toggle() {
-    if(document.getElementsByClassName('button')[0].textContent === 'More') {
-         document.getElementsByClassName('button')[0].textContent = 'Less';
-          document.getElementById('extra').style.display = 'block'
-    }else{
-        document.getElementsByClassName('button')[0].textContent = 'More';
-        document.getElementById('extra').style.display = 'none'
-    }
-}
